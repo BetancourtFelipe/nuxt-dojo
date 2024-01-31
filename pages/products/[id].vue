48339ec0d91eb07746194eb32acc562611ccca32
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-31 10:44:09 +0100

    Learn nuxt routes

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Products detail for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus praesentium quam sit temporibus voluptates repudiandae, dolores eum vitae similique placeat?
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
