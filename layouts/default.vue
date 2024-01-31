4d5d4ecd88eae04fe78bd87b01a824c9675fda05
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-31 11:33:56 +0100

    Add tailwind css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <header>
-      <div>
-        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
-        <ul>
+    <header class="shadow=sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+        <ul class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
           <li><NuxtLink to="/products">Products</NuxtLink></li>
         </ul>
-      </div>
+      </nav>
     </header>
 
     <!-- output the page content -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
   </div>
